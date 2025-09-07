5ed6b9da9426cc3ced61650373b5ce2413b844bf
Leonardo Menezes <limaleomenezes@gmail.com>
2025-09-07 18:17:45 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,26 +5,48 @@ git
 gnupg
 mpv
 picom
+readline
 redshift
 scripts
 sxhkd
 tmux
 vim
+xdg
 xorg
 zathura'
+stow_path="$HOME/.config/git
+$HOME/.config/mpv
+$HOME/.config/picom
+$HOME/.config/redshift
+$HOME/.config/sxhkd
+$HOME/.config/tmux
+$HOME/.config/vim
+$HOME/.config/zathura
+$HOME/.gnupg
+$HOME/.local/bin"
 
 case "$1" in
-	'link')
+	'base')
+		printf '%s\n' "$stow_path" | while IFS= read -r path; do
+			mkdir -p -v "$path"
+		done
+
+		curl -fLo ~/.config/vim/autoload/plug.vim --create-dirs \
+		    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+		git clone https://github.com/tmux-plugins/tpm ~/.config/tmux/plugins/tpm
+		;;
+	'stow')
 		printf '%s\n' "$stow_repo" | while IFS= read -r pack; do
 			stow -t $HOME -v 1 -S "$pack"
 		done
 		;;
-	'rm-link')
+	'stow-delete')
 		printf '%s\n' "$stow_repo" | while IFS= read -r pack; do
 			stow -t $HOME -v 1 -D "$pack"
 		done
 		;;
-	'relink')
+	'restow')
 		printf '%s\n' "$stow_repo" | while IFS= read -r pack; do
 			stow -t $HOME -v 1 -R "$pack"
 		done
